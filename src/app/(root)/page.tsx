05330b51c46e472a39cf485c1ecbe65a174a46d6
Tasmin Ansari <tasmin@verdant.co.in>
2024-01-07 12:14:06 +0530

    modified code

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,33 +1,12 @@
 'use client'
-// import AboutUs from "@/components/AboutUs";
-import dynamic from "next/dynamic";
-import React, { useEffect } from "react";
-import loadingGif from "@/assets/images/loading.gif";
 
-
-const AboutUs = dynamic(() => import("@/components/AboutUs"), {
-  loading: () => (
-    <div
-      style={{
-        height: "90vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <img src={loadingGif.src} alt="loading..." height={240} width={240} />
-    </div>
-  ),
-});
+import AboutUs from "@/components/AboutUs";
+import React from "react";
 
 export default function AboutUsPage() {
-
- 
-
   return (
     <>
-    
       <AboutUs />
-    </>
-  );
+    </>
+  );
 }
